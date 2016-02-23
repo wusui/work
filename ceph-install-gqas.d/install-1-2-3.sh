f8de664aa779d4aeaa2db8b76d9d46cf289def13
Warren Usui <wusui@redhat.com>
2016-02-22 19:53:03 -0800

    Add ceph health check

--- ceph-install-gqas.d/install-1-2-3.sh
+++ ceph-install-gqas.d/install-1-2-3.sh
@@ -5,7 +5,7 @@ CEPH_DEPLOY_OSDS=${CEPH_DEPLOY_OSDS:-"gqas006-priv gqas007-priv"}
 CEPH_DEPLOY_DISKS=${CEPH_DEPLOY_DISKS:-"sdb sdc sdd sde sdf sdg sdh sdi sdj sdk sdl sdm"}
 $CEPH_DEPLOY new $CEPH_DEPLOY_MONS
 if [ $? -ne 0 ]; then echo 'Error deploy new mons'; fi
-# read text
+read text
 $CEPH_DEPLOY install $CEPH_DEPLOY_MONS $CEPH_DEPLOY_OSDS
 if [ $? -ne 0 ]; then echo 'Error installing mons or osds'; fi
 $CEPH_DEPLOY mon create-initial
@@ -18,4 +18,9 @@ for i in $CEPH_DEPLOY_OSDS; do
         $CEPH_DEPLOY osd create $i:$dsk
         if [ $? -ne 0 ]; then echo 'Error creating disks'; fi
     done
+done
+x=`sudo ceph health`
+while [[ $x == HEALTH_OK* ]]; do
+    sleep 10
+    x=`sudo ceph health`
 done
