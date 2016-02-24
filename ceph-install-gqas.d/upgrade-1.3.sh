e328da3f1c216255adf87626f5a2c549d067ccc8
Warren Usui <wusui@redhat.com>
2016-02-23 21:25:56 -0800

    Top level 1.3 upgrade script

---
+++ ceph-install-gqas.d/upgrade-1.3.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+#
+CEPH_DEPLOY_MONS=${CEPH_DEPLOY_MONS:-"gqas005-priv"}
+CEPH_DEPLOY_OSDS=${CEPH_DEPLOY_OSDS:-"gqas006-priv gqas007-priv"}
+CEPH_DEPLOY_CLIENTS=${CEPH_DEPLOY_CLIENTS:-"gqac015-priv"}
+for i in $CEPH_DEPLOY_MONS; do
+    ./upgrade-mon.sh $i
+    ssh $i sudo /etc/init.d/ceph stop
+done
+for i in $CEPH_DEPLOY_OSDS; do
+    ./upgrade-osd.sh $i
+done
+for i in $CEPH_DEPLOY_CLIENTS; do
+    ./upgrade-clients.sh $i
+done
