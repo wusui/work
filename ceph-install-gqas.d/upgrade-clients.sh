a9b9f8d70fc863e7159290675c34477187e413f7
Warren Usui <wusui@redhat.com>
2016-02-24 16:55:33 -0800

    Install packages in right order

--- ceph-install-gqas.d/upgrade-clients.sh
+++ ceph-install-gqas.d/upgrade-clients.sh
@@ -2,7 +2,11 @@
 #
 sudo yum -y erase librados2
 CEPH_ISO=${CEPH_ISO:-"rhceph-1.3-rhel-7-x86_64-dvd.iso"}
-CEPH_HOSTNAME=`hostname`
 sudo umount /mnt
 sudo mount -o loop $CEPH_ISO /mnt
+sudo yum -y update
+sudo yum -y install /mnt/OSD/librados2*
+sudo yum -y install /mnt/OSD/librbd1*
+sudo yum -y install /mnt/OSD/python-rados*
+sudo yum -y install /mnt/OSD/python-rbd*
 sudo yum -y install /mnt/OSD/ceph-common*.rpm
