6e15e6ab4b0a41110393fd898a01d4f3e6615245
Warren Usui <wusui@redhat.com>
2016-02-24 16:58:31 -0800

    Minor fixes
    Add -y to yum erase command.
    Add ssh $1 to ceph quorum-status command

--- ceph-install-gqas.d/upgrade-mon.sh
+++ ceph-install-gqas.d/upgrade-mon.sh
@@ -2,7 +2,7 @@
 #
 ssh $1 sudo rm -rf /etc/yum.repos.d/ceph*.repo /etc/yum.repos.d/calamari*.repo
 ssh $1 sudo /etc/init.d/ceph stop
-ssh $1 sudo yum erase ceph-osd
+ssh $1 sudo yum -y erase ceph-osd
 ceph-deploy install --repo --release=ceph-mon $1
 ceph-deploy install --mon $1
 ssh $1 sudo /etc/init.d/ceph start
@@ -12,5 +12,5 @@ while [[ $x != HEALTH_OK* ]]; do
     sleep 10
     x=`ssh $1 sudo ceph health`
 done
-sudo ceph quorum-status --format json-pretty
+ssh $1 sudo ceph quorum-status --format json-pretty
 sleep 20
